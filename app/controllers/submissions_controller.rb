b59d0cc06240dc7409f37f799c2310acfaf4046f
aaronzomback <aaronzomback@gmail.com>
2020-12-18 00:56:40 +0100

    flash change color and dropdown profile

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -16,7 +16,7 @@ class SubmissionsController < ApplicationController
 
   else
 
-    flash[:error] = "You must be signed in to submit a film!"
+    flash[:error] = "Ah! You must have an account and be signed in to submit a film!"
     redirect_to new_session_path
 
   end
